0d31f07030d477b92cf7ec9ee1cdf129ca8f5b86
FarhanABD <farhanabdilah204@gmail.com>
2023-09-30 21:49:27 +0700

    Route fallback method

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,46 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('about',function(){
+//     return "<h1>About Page</h1>";
+// })->name('about');
+
+// Route::get('contact',function(){
+//     return "<h1>Contact Page</h1>";
+// });
+
+// Route::get('contact/{id}',function($id){
+//     return $id;
+// });
+
+// Route::get('home',function(){
+//     return "<a href='*.route('about').*'>About</a>";
+// });
+
+//---- ROUTE GROUPING CUSTOMER -----//
+Route::group(['prefix' => 'customer'], function(){
+    Route::get('/', function(){
+        return '<h1>Customer List<\h1>';
+    });
+    Route::get('/create', function(){
+        return '<h1>Customer create<\h1>';
+    });
+    Route::get('/create', function(){
+        return '<h1>Customer show<\h1>';
+    });
+});
+
+//--- ROUTE METHODS -------//
+//--- GET - Request a resource //
+//--- POST - Create a new resource //
+//--- PUT - Update a resource //
+//--- DELETE - Delete a resource //
+
+//----------- ROUTE FALLBACK ---------//
+Route::fallback(function(){
+    return "Route not exist";
 });
